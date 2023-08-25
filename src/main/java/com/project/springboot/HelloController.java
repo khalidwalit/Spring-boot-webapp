b9389c49d8a74e7ff0ba1e85973f7fc7b902b9da
Khalid Walit <harithkhalid93@gmail.com>
2023-08-25 19:29:51 +0800

    Update HelloController.java

--- src/main/java/com/project/springboot/HelloController.java
+++ src/main/java/com/project/springboot/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     @GetMapping("/")
     public String index() {
         String a = "";
-        return "Lambat  sdke!";
+        return "Sucessfully connected !!!";
     }
 
 }
